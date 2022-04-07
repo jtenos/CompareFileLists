229ee4e01a801835ff3e7ba5c4cea474fcaf4e04
Joe Enos <jtenos1@gmail.com>
2022-04-07 15:07:23 -0700

    Adding empty S3 project.

---
+++ CompareFileLists.S3/S3Source.cs
@@ -0,0 +1,12 @@
+﻿using CompareFileLists.Core;
+
+namespace CompareFileLists.S3;
+
+public class S3Source
+    : SourceBase
+{
+    protected override IAsyncEnumerable<ObjectInfo> EnumerateObjectsAsync()
+    {
+        throw new NotImplementedException();
+    }
+}

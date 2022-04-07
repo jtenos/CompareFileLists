08192b9c473f2fceb8377f54318baf8ffb84c372
Joe Enos <jtenos1@gmail.com>
2022-04-07 15:22:06 -0700

    Adding Azure Blob source.

---
+++ CompareFileLists.AzureBlob/AzureBlobSource.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using CompareFileLists.Core;
+
+namespace CompareFileLists.AzureBlob;
+
+public class AzureBlobSource
+    : SourceBase
+{
+    public AzureBlobSource(string connectionString, string containerName)
+    {
+        ConnectionString = connectionString;
+        ContainerName = containerName;
+    }
+
+    private string ConnectionString { get; }
+    private string ContainerName { get; }
+
+    protected override async IAsyncEnumerable<ObjectInfo> EnumerateObjectsAsync()
+    {
+        BlobContainerClient containerClient = new(ConnectionString, ContainerName);
+
+        foreach (BlobItem blob in containerClient.GetBlobs())
+        {
+            yield return new(blob.Name, blob.Properties.ContentLength ?? -1);
+        }
+        await Task.CompletedTask;
+    }
+}
--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -1,10 +1,19 @@
-﻿using CompareFileLists.Core;
+﻿using CompareFileLists.AzureBlob;
+using CompareFileLists.Core;
 using CompareFileLists.FileSystem;
 using MergeSortFile;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
 
 FileListComparison flc = new(logger: null, new LineSorter(logger: null));
 FileSystemSource source1 = new(@"C:\temp\dir1");
-FileSystemSource source2 = new(@"C:\temp\dir2");
+//FileSystemSource source2 = new(@"C:\temp\dir2");
+Console.Write("Connection String: ");
+string connectionString = Console.ReadLine() ?? "";
+Console.Write("Container Name: ");
+string containerName = Console.ReadLine() ?? "";
+AzureBlobSource source2 = new(connectionString, containerName);
 
 await foreach (Difference diff in flc.CompareSourcesAsync(source1, source2))
 {
